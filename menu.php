09076da898586fef93eb4fed17478a2e5832ceb2
umutalacam <umutcanalacam@gmail.com>
2020-12-12 21:34:08 +0300

    Create user menu UI

---
+++ menu.php
@@ -0,0 +1,110 @@
+<?php
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Restaurant - Menu</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/menu.css">
+</head>
+<body>
+
+
+<div class="menu-container">
+    <div class="menu-header">
+        <img class="menu-header-logo" src="assets/img/logo.jpg" />
+    </div>
+
+    <div class="submenu-container">
+        <div class="submenu-header"> <h2>Hot Beverages</h2> </div>
+
+        <div class="product-container">
+            <div class="product-info-row">
+                <div class="product-name"><h3>Turkish tea</h3></div>
+                <div class="product-price">$ 3.50</div>
+            </div>
+            <div class="product-detail-row">
+                <div class="product-description">Tradtional Turkish black tea</div>
+                <div class="product-actions">
+                    <button class="action-btn-remove">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                    <button class="action-btn-add">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="product-container">
+            <div class="product-info-row">
+                <div class="product-name"><h3>Espresso</h3></div>
+                <div class="product-price">$ 3.50</div>
+            </div>
+            <div class="product-detail-row">
+                <div class="product-description">Tradtional Turkish black tea</div>
+                <div class="product-actions">
+                    <button class="action-btn-remove">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                    <button class="action-btn-add">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="product-container">
+            <div class="product-info-row">
+                <div class="product-name"><h3>Latte</h3></div>
+                <div class="product-price">$ 3.50</div>
+            </div>
+            <div class="product-detail-row">
+                <div class="product-description">Tradtional Turkish black tea</div>
+                <div class="product-actions">
+                    <button class="action-btn-remove">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                    <button class="action-btn-add">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="submenu-container">
+        <div class="submenu-header"> <h2>Pizza</h2> </div>
+
+        <div class="product-container">
+            <div class="product-info-row">
+                <div class="product-name"><h3>Turkish tea</h3></div>
+                <div class="product-price"><span class="product-quantity">Qty: 2</span> $ 3.50</div>
+            </div>
+            <div class="product-detail-row">
+                <div class="product-description">Tradtional Turkish black tea</div>
+                <div class="product-actions">
+                    <button class="action-btn-remove">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                    <button class="action-btn-add">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<!-- Scripts -->
+<script src="js/vendor/jquery-3.5.1.slim.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
