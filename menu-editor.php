a986be58200623742a1252bd76ded6d4984ee9bb
Cgokgoz <49099276+Cgokgoz@users.noreply.github.com>
2020-12-13 21:30:58 +0300

    Add files via upload

---
+++ menu-editor.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/menu-editor.js"></script>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Menu</title>
+    <link href="css/styles.css" rel="stylesheet"/>
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+          crossorigin="anonymous"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/menu-editor.css">
+</head>
+<body class="sb-nav-fixed">
+<?php include "inc/nav.php"?>
+<div id="layoutSidenav">
+    <?php include "inc/sidebar.php"?>
+    <div id="layoutSidenav_content">
+        <div class="grid-container" >
+            <div class="grid-container" id="main">
+                <button id="category_button" class="button new_category_button">Create New Category</button>
+            </div>
+        </div>
+
+    </div>
+</div>
+</body>
