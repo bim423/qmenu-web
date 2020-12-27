aa6fc3c43acba520f9883a62a0fbf571ef71e7ae
umutalacam <umutcanalacam@gmail.com>
2020-12-27 22:21:10 +0300

    Create order terminal UI

--- order-terminal.php
+++ order-terminal.php
@@ -8,6 +8,8 @@
         <meta name="author" content="" />
         <title>Order Terminal - QR Menu</title>
         <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/custom.css" rel="stylesheet" />
+        <link href="css/order-terminal.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -19,20 +21,184 @@
                 <main>
                     <div class="container-fluid">
                         <h1 class="mt-4">Order Terminal</h1>
-                        <div class="card mb-4">
-                            <div class="card-body">
-                                <p class="mb-0">
-                                    Order terminal will be in here
-                                </p>
+                        <div class="order-terminal-container">
+                            <div id="order-terminal">
+                                <div id="pending-orders">
+                                    <div class="order-container order-container-danger" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">Outdoor-3</div>
+                                            <div class="order-arrival-time">25 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            2x Tea<br>
+                                            1x Espresso<br>
+                                            1x Americano<br>
+                                            1x Cold Brew
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-danger">Late</span>
+                                            <span class="badge badge-warning">Pending</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
+                                            <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container order-container-warning" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">7 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Margarita Pizza<br>
+                                            2x Napolitana Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-warning">Pending</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
+                                            <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container order-container-warning" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A3</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            2x Black Tea<br>
+                                            2x Croissant
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-warning">Pending</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
+                                            <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container order-container-warning" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A2</div>
+                                            <div class="order-arrival-time">1 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Americano<br>
+                                            1x Cold Brew<br>
+                                            1x Green Tea<br>
+                                            1x Black Tea<br>
+                                            2x Croissant
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-warning">Pending</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
+                                            <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div id="processing-orders">
+                                    <div class="order-container" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-success">Processing</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                                            <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-success">Processing</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                                            <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-success">Processing</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                                            <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-success">Processing</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                                            <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
+                                        </div>
+                                    </div>
+                                    <div class="order-container" data-order-id="1">
+                                        <div class="order-desk-col">
+                                            <div class="order-desk-name">A1</div>
+                                            <div class="order-arrival-time">5 mins ago</div>
+                                        </div>
+                                        <div class="order-detail-col">
+                                            1x Pizza
+                                        </div>
+                                        <div class="order-badge-col">
+                                            <span class="badge badge-success">Processing</span>
+                                        </div>
+
+                                        <div class="order-action-col">
+                                            <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                                            <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </main>
-                <?php include "inc/footer.php"?>
             </div>
         </div>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="js/vendor/jquery.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/controller/order-terminal.js"></script>
         <script src="js/scripts.js"></script>
     </body>
 </html>
