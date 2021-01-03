8998fe63b2156f7a9272bccb3a0e4fb79ed4673c
umutalacam <umutcanalacam@gmail.com>
2021-01-03 15:18:57 +0300

    Update order terminal controller for new API

--- js/controller/order-terminal.js
+++ js/controller/order-terminal.js
@@ -7,6 +7,9 @@ $(document).ready(function () {
     listenAPI();
 })
 
+/**
+ * Check state of the orders asynchronously
+ */
 async function listenAPI() {
     $.ajax({
         url: API.ORDER,
@@ -31,157 +34,146 @@ function updateTerminalUI(orders) {
     let processingOrdersContainer = $("#processing-orders");
 
     // Categorize orders
-    for (let i=0; i<orders.length; i++) {
+    for (let i = 0; i < orders.length; i++) {
         let order = orders[i];
         // If this order is already delivered ignore it
         if (order.state === DELIVERED_ORDER) continue;
 
         // Extract order data
-        if (order.state === PENDING_ORDER) {
-            pendingOrders.push(order)
-        } else if (order.state === PROCESSING_ORDER) {
-            processingOrders.push(order)
-        }
+        if (order.state === PENDING_ORDER) pendingOrders.push(order)
+        else if (order.state === PROCESSING_ORDER) processingOrders.push(order)
     }
 
     // Render pending orders
     let pendingOrdersContent = ""
-    for (let i=0; i<pendingOrders.length; i++) {
+    for (let i = 0; i < pendingOrders.length; i++) {
         let order = pendingOrders[i];
-        pendingOrdersContent += renderPendingOrder(order.id, order.arrivalTime, order.deskId, order.orderedProducts);
+        //pendingOrdersContent += renderPendingOrder(order.id, order.arrivalTime, order.deskLabel, order.orderedProducts);
+        pendingOrdersContent += renderOrderContainer(order.id, order.arrivalTime, order.deskLabel, order.orderedProducts, PENDING_ORDER);
     }
     pendingOrdersContainer.html(pendingOrdersContent);
 
     // Set actions
     $(`.order-container .order-btn-approve`).click(function (e) {
-        console.log(e.currentTarget)
-        let orderId = e.currentTarget.parentNode.parentNode.dataset.orderId;
-        console.log("Approve order: "+ orderId);
+        let orderContainer = e.currentTarget.parentNode.parentNode;
+        let orderId = orderContainer.dataset.orderId;
+        orderContainer.style = "opacity: 0.4";
+        console.log("Approve order: " + orderId);
         setOrderState(orderId, PROCESSING_ORDER);
     })
 
     $(`.order-container .order-btn-deny`).click(function (e) {
-        console.log(e.currentTarget)
-        let orderId = e.currentTarget.parentNode.parentNode.dataset.orderId;
-        console.log("Delete order: "+ orderId);
+        let orderContainer = e.currentTarget.parentNode.parentNode;
+        let orderId = orderContainer.dataset.orderId;
+        orderContainer.style = "opacity: 0.4";
         deleteOrder(orderId);
+        console.log("Delete order: " + orderId);
     })
 
 
     // Render processing orders
     let processingOrdersContent = ""
-    for (let i=0; i<processingOrders.length; i++) {
+    for (let i = 0; i < processingOrders.length; i++) {
         let order = processingOrders[i];
-        processingOrdersContent += renderProcessingOrder(order.id, order.arrivalTime, order.deskId, order.orderedProducts);
+        // processingOrdersContent += renderProcessingOrder(order.id, order.arrivalTime, order.deskLabel, order.orderedProducts);
+        processingOrdersContent += renderOrderContainer(order.id, order.arrivalTime, order.deskLabel, order.orderedProducts, PROCESSING_ORDER);
+
     }
     processingOrdersContainer.html(processingOrdersContent);
 
     // Set button events
     $(`.order-container .order-btn-deliver`).click(function (e) {
-        let orderId = e.currentTarget.parentNode.parentNode.dataset.orderId;
-        console.log("Set order state delivered: "+ orderId);
+        let orderContainer = e.currentTarget.parentNode.parentNode;
+        let orderId = orderContainer.dataset.orderId;
+        orderContainer.style = "opacity: 0.4";
         setOrderState(orderId, DELIVERED_ORDER);
+        console.log("Set order state delivered: " + orderId);
     })
 
     $(`.order-container .order-btn-revert`).click(function (e) {
-        let orderId = e.currentTarget.parentNode.parentNode.dataset.orderId;
-        console.log("Revert order: "+ orderId);
+        let orderContainer = e.currentTarget.parentNode.parentNode;
+        let orderId = orderContainer.dataset.orderId;
+        orderContainer.style = "opacity: 0.4";
         setOrderState(orderId, PENDING_ORDER);
+        console.log("Revert order: " + orderId);
     })
 }
 
 /**
- * Create a pending order card html
+ * Build order container content
  * @param orderId
  * @param arrivalTime
- * @param deskName
+ * @param deskLabel
  * @param products
- * @returns {string} html code of card
+ * @param state
+ * @returns {string}
  */
-function renderPendingOrder(orderId, arrivalTime, deskName, products) {
+function renderOrderContainer(orderId, arrivalTime, deskLabel, products, state) {
+    // Generate order content
     let orderContent = ""
-    for (let i = 0; i<products.length; i++) {
+    for (let i = 0; i < products.length; i++) {
         let product = products[i];
-        orderContent += product.quantity + "x " + product.name;
+        orderContent += product.quantity + "x " + product.productName + "<br>";
     }
 
+    // Generate human friendly time string
     let timePassed = formatTimePassed(arrivalTime);
     let labelTimePassed = "";
-
 
     if (Number(timePassed) === 0) labelTimePassed = "Just now";
     else labelTimePassed = timePassed + " minutes ago";
 
-    let className = "warning";
-    let badgesContent = `<span class="badge badge-warning">Pending</span>`;
-    if (Number(timePassed) >= 10) {
-        className = "danger";
-        badgesContent = `<span class="badge badge-danger mr-2">Late</span>` + badgesContent;
+    // Generate badges content, container class and actions
+    let className = ""
+    let badgesContent = ""
+    let actionsContent = ""
+
+    if (state === PENDING_ORDER) {
+        className = "order-container-warning";
+        badgesContent = `<span class="badge badge-warning">Pending</span>`;
+
+        if (Number(timePassed) >= 10) {
+            className = "order-container-danger";
+            badgesContent = `<span class="badge badge-danger mr-2">Late</span>` + badgesContent;
+        }
+
+        actionsContent = `
+                <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
+                <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>`;
+
+    } else if (state === PROCESSING_ORDER) {
+        badgesContent = `<span class="badge badge-success">Processing</span>`
+
+        if (Number(timePassed) >= 15) {
+            badgesContent = `<span class="badge badge-danger mr-2">Late</span>` + badgesContent;
+        }
+
+        actionsContent = `
+                <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
+                <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>`;
+
     }
 
-    // noinspection UnnecessaryLocalVariableJS
-    let domOutput = `
-        <div class="order-container order-container-${className}" data-order-id="${orderId}">
+    let orderContainerContent = `
+        <div class="order-container ${className}" data-order-id="${orderId}">
             <div class="order-desk-col">
-                <div class="order-desk-name">${deskName}</div>
+                <div class="order-desk-name">${deskLabel}</div>
                 <div class="order-arrival-time">${labelTimePassed}</div>
             </div>
-            <div class="order-detail-col">
-                ${orderContent}
-            </div>
-            <div class="order-badge-col">
-                ${badgesContent}
-            </div>
-
-            <div class="order-action-col">
-                <button class="btn btn-link order-btn-deny"><span class="fa fa-times"></span></button>
-                <button class="btn btn-link order-btn-approve"><span class="fa fa-check"></span></button>
-            </div>
+            <div class="order-detail-col">${orderContent}</div>
+            <div class="order-badge-col">${badgesContent}</div>
+            <div class="order-action-col">${actionsContent}</div>
         </div>
     `
-    return domOutput;
+
+    return orderContainerContent
 }
 
 /**
- * Create a processing order card html
- * @param orderId
- * @param arrivalTime
- * @param deskName
- * @param products
- * @returns {string} html code of card
+ * Make API Request and set order state to any other state
+ * @param {int} orderId
+ * @param {int} state
  */
-function renderProcessingOrder(orderId, arrivalTime, deskName, products) {
-    let orderContent = ""
-    let time = arrivalTime;
-    for (let i = 0; i<products.length; i++) {
-        let product = products[i];
-        orderContent += product.quantity + "x " + product.name;
-    }
-
-    // noinspection UnnecessaryLocalVariableJS
-    let domOutput = `
-        <div class="order-container" data-order-id="${orderId}">
-            <div class="order-desk-col">
-                <div class="order-desk-name">${deskName}</div>
-                <div class="order-arrival-time">${time}</div>
-            </div>
-            <div class="order-detail-col">
-                ${orderContent}
-            </div>
-            <div class="order-badge-col">
-                <span class="badge badge-success">Processing</span>
-            </div>
-
-            <div class="order-action-col">
-                <button class="btn btn-link order-btn-revert"><span class="fa fa-undo"></span></button>
-                <button class="btn btn-link order-btn-deliver"><span class="fa fa-location-arrow"></span></button>
-            </div>
-        </div>
-    `
-
-    return domOutput;
-}
-
 function setOrderState(orderId, state) {
     let requestBody = {
         id: orderId,
@@ -199,6 +191,10 @@ function setOrderState(orderId, state) {
     })
 }
 
+/**
+ * Make API request and delete order
+ * @param {int} orderId
+ */
 function deleteOrder(orderId) {
     $.ajax({
         url: API.ORDER + "/delete/" + orderId,
@@ -210,12 +206,17 @@ function deleteOrder(orderId) {
     })
 }
 
+/**
+ * Generate minutes string from the arrival time string
+ * @param {string} dateTime
+ * @returns {string}
+ */
 function formatTimePassed(dateTime) {
     let dateNow = new Date()
     let date = new Date(dateTime)
     let timePassed = dateNow.getTime() - date.getTime()
 
-    let minutesPassed = timePassed / (1000*60)
+    let minutesPassed = timePassed / (1000 * 60)
     minutesPassed = Number(minutesPassed).toFixed(0)
     return minutesPassed;
 }
