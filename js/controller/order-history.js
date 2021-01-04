45012e192be56341eed29e4e77bfbc0b3d41efa2
umutalacam <umutcanalacam@gmail.com>
2021-01-04 12:31:31 +0300

    Add order history controller

--- js/controller/order-history.js
+++ js/controller/order-history.js
@@ -1,19 +1,115 @@
+let menu;
+let orders;
+
 $(document).ready(function () {
-
-
-    // Add event to the each table row
-    $("#order-history-table ").on('click', 'tr', function (e) {
-        let target = e.currentTarget;
-        console.log(target)
-        let orderId = target.dataset.orderId;
-        if (orderId) {
-            console.log("showing modal " + orderId)
-            showDetailOrderDialog(orderId);
-        }
-    });
+    // Get restaurant menu
+    getMenu()
 })
+
+function getMenu() {
+    // Get menu from rest API
+    $.ajax({
+        url: API.MENU,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+            menu = data;
+            getOrders()
+        }
+    })
+}
+
+function getOrders() {
+    // Get orders from rest API
+    $.ajax({
+        url: API.ORDER,
+        type: 'GET',
+        contentType: 'application/json',
+        success: function (data) {
+            orders = data;
+            buildTable(orders)
+        }
+    })
+}
+
+function buildTable(orders) {
+    let order_history_table = $("#order-history-table tbody")
+
+    let tableBodyContent = ``;
+    for(let i=0; i<orders.length; i++) {
+        let order = orders[i];
+        let orderDetail = formatOrderDetail(order.orderedProducts);
+        let cost = calculateCost(order.orderedProducts)
+        let date = formatDate(order.arrivalTime)
+        let state = formatState(order.state)
+
+        tableBodyContent += `
+            <tr data-order-id="${order.id}">
+                <td class="order-desk-label">${order.deskLabel}</td>
+                <td class="order-detail">${orderDetail}</td>
+                <td>${date}</td>
+                <td class="order-state">${state}</td>
+                <td class="order-cost">$ ${Number(cost).toFixed(2)}</td>
+            </tr>
+            
+        `;
+
+        }
+
+    order_history_table.append(tableBodyContent)
+    $("#order-history-table").DataTable({autoWidth: false})
+}
+
+function calculateCost(orderedProducts) {
+    let cost = 0;
+    for (let i=0; i<orderedProducts.length; i++) {
+        let orderedProduct = orderedProducts[i];
+        let price = getProductCostById(orderedProduct.productId);
+        if (price !== undefined)
+            cost += price
+    }
+    return cost;
+}
+
+function getProductCostById(productId) {
+    let subMenus = menu.menu
+    for (let i=0; i<subMenus.length; i++) {
+        let subMenuProducts = subMenus[i].products;
+        for (let j=0; j<subMenuProducts.length; j++) {
+            let product = subMenuProducts[j];
+            if (product.id === productId) {
+                return product.price;
+            }
+        }
+    }
+    return undefined
+}
+
+function formatDate(arrivalTime) {
+    return arrivalTime;
+}
+
+function formatState(state) {
+    if (state === 0) {
+        return `<span class="text-danger">Pending</span>`
+    } else if (state === 1) {
+        return `<span class="text-warning">Processing</span>`
+    } else{
+        return `<span class="text-success">Delivered</span>`
+    }
+}
+
+function formatOrderDetail(orderedProducts) {
+    let orderDetailContent = ``;
+    for (let i=0; i<orderedProducts.length; i++) {
+        let orderedProduct = orderedProducts[i];
+        orderDetailContent += `${orderedProduct.quantity}x ${orderedProduct.productName}<br>`
+    }
+    return orderDetailContent
+}
+
 /**
- *TODO:
+ *TODO: Not needed yet
  */
 function showDetailOrderDialog(orderId){
     showModalDialog("Order Details",
--- order-history.php
+++ order-history.php
@@ -8,6 +8,8 @@
     <meta name="author" content="" />
     <title>Order Terminal - QR Menu</title>
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/custom.css" rel="stylesheet" />
+    <link href="css/order-history.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
@@ -19,84 +21,20 @@
         <main>
             <div class="container-fluid">
                 <h1 class="mt-4">Order History</h1>
-                <div class="col-xl-12">
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <i class="fas fa-table mr-1"></i>
-                            Recent Orders
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive table-recent-orders">
-                                <table class="table" id="order-history-table">
-                                    <tbody>
-                                    <tr>
-                                        <th>Table</th>
-                                        <th>Order</th>
-                                        <th>Date</th>
-                                        <th>Status</th>
-                                        <th>Total cost</th>
-                                    </tr>
-                                    <tr data-order-id="1">
-                                        <td>A1</td>
-                                        <td class="order-detail" data-toggle="tooltip" data-placement="bottom"
-                                            title="Turkish Tea 2.5 TL x 2 = 5 TL , Margarita Pizza 40 TL ">
-                                            2x Turkish Tea<br>
-                                            1x Margarita Pizza
-                                        </td>
-                                        <td>12.01.2020 <br>
-                                        13.30</td>
-                                        <td class="text-danger order-status">Pending</td>
-                                        <td class="order-cost">$ 45</td>
-                                    </tr>
-                                    <tr data-order-id="2">
-                                        <td>A2</td>
-                                        <td class="order-detail" data-toggle="tooltip" data-placement="bottom"
-                                            title="Café Americano 20 TL x 2 = 40 TL , Turkish Tea 2.5 TL ">
-                                            2x Café Americano<br>
-                                            1x Turkish Tea</td>
-                                        <td>12.01.2020 <br>
-                                            13.45</td>
-                                        <td class="text-success order-status">Delivered</td>
-                                        <td class="order-cost">$ 45</td>
-                                    </tr>
-                                    <tr data-order-id="3">
-                                        <td>A4</td>
-                                        <td class="order-detail" data-toggle="tooltip" data-placement="bottom"
-                                            title="Royal Pizza 20 TL , Margarita Pizza 40 TL , Napolitana Pizza 15 TL ">
-                                            1x Royal Pizza<br>
-                                            1x Margarita Pizza<br>
-                                            1x Napolitana Pizza
-                                        </td>
-                                        <td>12.01.2020 <br>
-                                            14.00</td>
-                                        <td class="text-warning order-status">Processing</td>
-                                        <td class="order-cost">$ 75</td>
-                                    </tr>
-                                    <tr data-order-id="4">
-                                        <td>A5</td>
-                                        <td class="order-detail" data-toggle="tooltip" data-placement="bottom"
-                                            title="Café Americano 20 TL x 2 = 40 TL ">
-                                            2x Café Americano</td>
-                                        <td>12.01.2020 <br>
-                                            14.15</td>
-                                        <td class="text-success order-status">Delivered</td>
-                                        <td class="order-cost">$ 15</td>
-                                    </tr>
-                                    <tr data-order-id="5">
-                                        <td>B2</td>
-                                        <td class="order-detail" data-toggle="tooltip" data-placement="bottom"
-                                            title="Turkish Tea 2.5 TL x 2 = 5 TL ">
-                                            2x Turkish Tea<br>
-                                        <td>12.01.2020 <br>
-                                            14.30</td>
-                                        <td class="text-success order-status">Delivered</td>
-                                        <td class="order-cost">$ 5</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
+                <div class="table-responsive table-order-history" style="width: 100%">
+                    <table class="table table-bordered" id="order-history-table">
+                        <thead>
+                        <tr>
+                            <th>Table</th>
+                            <th>Order</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th>Total cost</th>
+                        </tr>
+                        </thead>
+
+                        <tbody></tbody>
+                    </table>
                 </div>
             </div>
         </main>
@@ -104,15 +42,13 @@
     </div>
 </div>
 <!--Scripts-->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="js/vendor/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="assets/demo/chart-area-demo.js"></script>
-<script src="assets/demo/chart-bar-demo.js"></script>
-<script src="assets/demo/chart-pie-demo.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="js/api.js"></script>
+<script src="js/scripts.js"></script>
 <script src="js/addons/modals.js"></script>
 <script src="js/controller/order-history.js"></script>
-<script src="js/scripts.js"></script>
 </body>
 </html>
